7abfccb58e4b337fc0896fe23624d4411a8c96d8
chrisdews <cldews1@gmail.com>
2019-11-01 14:09:24 +0000

    multiply done

--- src/recursion.js
+++ src/recursion.js
@@ -97,16 +97,21 @@ var palindrome = function(string) {
 // modulo(22,6) // 4
 var modulo = function(x, y) {
   if (y === 0) return NaN;
-  // convert negative cases to positive
+
   if (x < 0) return -modulo(-x, y);
   if (y < 0) return modulo(x, -y);
+
   if (x < y) return x;
+
   return modulo(x - y, y);
 };
 
 // 12. Write a function that multiplies two numbers without using the * operator or
 // Math methods.
-var multiply = function(x, y) {};
+var multiply = function(x, y) {
+    if (x===0 || y===0) return 0
+    return y < 0 ? -x + multiply(x, y+1) : x + multiply(x, y-1)
+};
 
 // 13. Write a function that divides two numbers without using the / operator or
 // Math methods to arrive at an approximate quotient (ignore decimal endings).
