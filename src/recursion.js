6e918d0c2f145bd0ffbd3df05349c52b53741067
chrisdews <cldews1@gmail.com>
2019-11-03 23:43:32 +0000

    countOccurence done

--- src/recursion.js
+++ src/recursion.js
@@ -190,7 +190,11 @@ var fizzBuzz = function(n) {
 // 20. Count the occurence of a value in a list.
 // countOccurrence([2,7,4,4,1,4], 4) // 3
 // countOccurrence([2,'banana',4,4,1,'banana'], 'banana') // 2
-var countOccurrence = function(array, value) {};
+var countOccurrence = function(array, value) {
+    if (array.length === 0) return 0
+    return array[0]===value ?  1 + countOccurrence(array.slice(1), value) : countOccurrence(array.slice(1), value)
+    
+};
 
 // 21. Write a recursive version of map.
 // rMap([1,2,3], timesTwo); // [2,4,6]
